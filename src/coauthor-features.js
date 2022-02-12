164d23017e0d823e4cd7548dee14b7ec09485327
hk <hammadkhanxcm@gmail.com>
2022-02-12 10:00:00 +0500

    Week 7: Add collaborative feature
    Co-authored-by: hammadxcm2 <hammadkhanxcm@icloud.com>
    
    Co-authored-by: 12129046-dev <238371180+12129046-dev@users.noreply.github.com>

--- src/coauthor-features.js
+++ src/coauthor-features.js
@@ -4,3 +4,4 @@ function weekFeature3() { return 'Week 3 feature from 2022'; }
 function weekFeature4() { return 'Week 4 feature from 2022'; }
 function weekFeature5() { return 'Week 5 feature from 2022'; }
 function weekFeature6() { return 'Week 6 feature from 2022'; }
+function weekFeature7() { return 'Week 7 feature from 2022'; }
