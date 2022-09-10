0ba6b6b77ff85ba9bb9c45d3cca95b3ec1287c27
hk <hammadkhanxcm@gmail.com>
2022-09-10 10:00:00 +0500

    Week 37: Add collaborative feature
    Co-authored-by: hammadxcm2 <hammadkhanxcm@icloud.com>
    
    Co-authored-by: 12129046-dev <238371180+12129046-dev@users.noreply.github.com>

--- src/coauthor-features.js
+++ src/coauthor-features.js
@@ -31,3 +31,4 @@ function weekFeature32() { return 'Week 32 feature from 2022'; }
 function weekFeature33() { return 'Week 33 feature from 2022'; }
 function weekFeature34() { return 'Week 34 feature from 2022'; }
 function weekFeature35() { return 'Week 35 feature from 2022'; }
+function weekFeature37() { return 'Week 37 feature from 2022'; }
