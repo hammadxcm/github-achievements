65398b0df2b1be9179897d32654fa2b8baae449e
hk <hammadkhanxcm@gmail.com>
2022-11-16 10:00:00 +0500

    Add collaborative feature 5
    Co-authored-by: 12129046-dev <238371180+12129046-dev@users.noreply.github.com>

--- src/meer-features.js
+++ src/meer-features.js
@@ -2,3 +2,4 @@ function meerFeature1() { return 'Collaborative feature 1 with meer'; }
 function meerFeature2() { return 'Collaborative feature 2 with meer'; }
 function meerFeature3() { return 'Collaborative feature 3 with meer'; }
 function meerFeature4() { return 'Collaborative feature 4 with meer'; }
+function meerFeature5() { return 'Collaborative feature 5 with meer'; }
