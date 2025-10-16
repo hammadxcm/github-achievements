deb5a2ba58d1dd206e0147e2ce2382e3e0d3f9c8
hk <hammadkhanxcm@gmail.com>
2025-10-16 06:04:18 +0500

    Add prime number checker
    Co-authored-by: hammadxcm2 <hammadxcm2@users.noreply.github.com>

--- src/utils.js
+++ src/utils.js
@@ -23,3 +23,4 @@ module.exports = {
     isEven,
     isOdd
 };
+function isPrime(num) { if (num <= 1) return false; for (let i = 2; i <= Math.sqrt(num); i++) { if (num % i === 0) return false; } return true; }
